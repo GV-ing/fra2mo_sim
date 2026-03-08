d26559ae5a4de42ce56d03244ff3a7ba675d92d6
GV-ing <g.vestri2904@gmaiol.com>
2026-03-09 00:11:58 +0100

    Initial commit: Added fra2mo_sim project

--- docker_scripts/docker_build_image.sh
+++ docker_scripts/docker_build_image.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-
 # Start the Docker image build
 rm -rf ./tmp_sources
 mkdir -p ./tmp_sources
-cp -r ../src/* ./tmp_sources
+
+# Ensure source files exist before copying
+if [ -d "../src" ]; then
+  cp -r ../src/* ./tmp_sources
+else
+  echo "Error: Source directory ../src does not exist. Exiting."
+  exit 1
+fi
 
 docker build -t robot_description -f ./Dockerfile .
 
